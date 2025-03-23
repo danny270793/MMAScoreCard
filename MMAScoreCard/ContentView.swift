1c7c80dc5e738612a50b9aa90179b1812c75c273
Danny Vaca <danny270793@icloud.com>
2025-03-23 17:42:24 -0500

    list events

--- MMAScoreCard/ContentView.swift
+++ MMAScoreCard/ContentView.swift
@@ -13,52 +13,88 @@ struct ContentView: View {
     
     @State private var isFetching: Bool = true
     @State private var error: Error? = nil
-    @Query var events: [Event]
+    @State private var searchText = ""
+    @Query(sort: \Event.date, order: .reverse) var events: [Event]
+    
+    func onRefresh() {
+        Task {
+            isFetching = true
+            do {
+               let newEvents = try await Sheredog.loadEvents()
+                
+                print("deleting old \(events.count) events")
+                for event in events {
+                    context.delete(event)
+                }
+                
+                print("inserting new \(newEvents.count) events")
+                for event in newEvents {
+                    context.insert(event)
+                }
+                
+                print("saving to database")
+                try context.save()
+            } catch {
+                self.error = error
+            }
+            isFetching = false
+        }
+    }
+    
+    private var filteredEvents: [Event] {
+            if searchText.isEmpty {
+                return events
+            } else {
+                return events.filter { event in
+                    event.name.lowercased().contains(searchText.lowercased())
+                }
+            }
+        }
     
     var body: some View {
-        ZStack {
-            List(events) { event in
-                Text(event.name)
+        NavigationView {
+            List(filteredEvents) { event in
+                NavigationLink(destination: Text(event.name)) {
+                    VStack {
+                        Text(event.name.split(separator: "-")[0])
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        if event.fight != nil {
+                            Text(event.fight!)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        Text(event.date.ISO8601Format().split(separator: "T")[0])
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
             }
-            if isFetching {
-                ProgressView()
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Text("\(events.count) events")
+                }
             }
-        }
-        .alert(isPresented: .constant(error != nil)) {
-            Alert(
-                title: Text("Error"),
-                message: Text(error!.localizedDescription),
-                dismissButton: .default(Text("OK"))
-            )
-        }
-        .onAppear {
-             Task {
-                 isFetching = true
-                 do {
-                     let newEvents = try await Sheredog.loadEvents()
-                     print("\(newEvents.count) newEvents")
-                     
-                     print("deleting old events")
-                     for event in events {
-                         context.delete(event)
-                     }
-                     try context.save()
-                     
-                     print("inserting new events")
-                     for event in newEvents {
-                         context.insert(event)
-                     }
-                     try context.save()
-                     
-                 } catch {
-                     self.error = error
-                 }
-                 isFetching = false
-             }
+            .overlay {
+                if isFetching {
+                    ProgressView()
+                }
+            }
+            .alert(isPresented: .constant(error != nil)) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(error!.localizedDescription),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+            .onAppear(perform: onRefresh)
+            .refreshable(action: onRefresh)
+            .searchable(text: $searchText)
+            .navigationTitle("Events")
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: [
+            Event.self
+        ])
 }
