6381486142a922718be926b4dd327a52e63d5a77
Danny Vaca <danny270793@icloud.com>
2025-03-23 17:42:06 -0500

    add new field

--- MMAScoreCard/Models/Event.swift
+++ MMAScoreCard/Models/Event.swift
@@ -12,12 +12,14 @@ import SwiftData
 class Event {
     @Attribute(.unique) var id: UUID
     var name: String
+    var fight: String?
     var location: String
     var date: Date
     
-    init(id: UUID = UUID(), name: String, location: String, date: Date) {
+    init(id: UUID = UUID(), name: String, fight: String?, location: String, date: Date) {
         self.id = id
         self.name = name
+        self.fight = fight
         self.location = location
         self.date = date
     }
