9422b43b2eadfb310b8f8640483d4b82a674b5df
Danny Vaca <danny270793@icloud.com>
2025-03-25 13:06:47 -0500

    base version

--- web/src/pages/event.tsx
+++ web/src/pages/event.tsx
@@ -1,13 +1,45 @@
 import { useParams } from "react-router-dom";
+import { Event, Fight, Sherdog } from "../services/sherdog";
+import { useEffect, useState } from "react";
 
-const Event = () => {
-  const { id } = useParams();
+const EventPage = () => {
+  const { name } = useParams();
+
+  const [fights, setFights] = useState<Fight[]>([])
+
+  const init = async () => {
+    const event: Event = await Sherdog.getEvent(name!)
+    const fights: Fight[] = await Sherdog.getFights(event)
+    setFights(fights)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <div>
-      <h2>Event {id}</h2>
+      <h2>Event {name}</h2>
+            <ul>
+              {fights.map((fight: Fight, index: number) => (
+                <li key={index}>
+                  <div>
+                  {fight.fighter1.name} vs {fight.fighter2.name}
+                  </div>
+                  <div>
+                  {fight.division}
+                  </div>
+                  <div>
+                  {fight.method} - {fight.referee}
+                  </div>
+                  <div>
+                  Round {fight.round} at {fight.time}
+                  </div>
+                </li>
+              ))}
+            </ul>
     </div>
   );
 };
 
-export default Event;
+export default EventPage;
--- web/src/pages/events.tsx
+++ web/src/pages/events.tsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Event, Sherdog } from "../services/sherdog";
 
-const Events = () => {
+const EventsPage = () => {
+  const [events, setEvents] = useState<Event[]>([])
+
+  const init = async () => {
+    const events: Event[] = await Sherdog.getEvents()
+    setEvents(events)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
   return (
     <div>
       <h2>Events</h2>
       <ul>
-        <li>
-          <Link to="/events/1">
-            Event 1
-          </Link>
-        </li>
+        {events.map((event: Event, index: number) => (
+          <li key={index}>
+            {event.figth && <div>
+              {event.figth}
+            </div>}
+            <div>
+              {event.name}
+            </div>
+            <div>
+              {event.date.toISOString().split('T')[0]}
+            </div>
+            <Link to={`/events/${event.name}`}>
+              see more
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default Events;
+export default EventsPage;
