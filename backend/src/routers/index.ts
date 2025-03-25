a020290052312136c5c38a10c7bd51c1929b9175
Danny Vaca <danny270793@icloud.com>
2025-03-25 14:16:06 -0500

    expose data in the api

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { Event, Fight, Sherdog } from "../libraries/sherdog";
 import { Cache } from "../libraries/cache";
 import { FileCache } from "../libraries/cache/file-cache";
+import { error } from "node:console";
 
 export const router: Router = Router()
 
@@ -10,15 +11,25 @@ const cache: Cache = new FileCache('.cached.json')
 sherdog.setCache(cache)
 
 router.get('/events/:name', async (request: Request, response: Response<Fight[]>) => {
-    const events: Event[] = await sherdog.getEvents()
-    const filteredEvents: Event[] = events.filter((event: Event) => event.name === request.params.name)
-    if(filteredEvents.length !== 1) {
-        response.status(404).json()
+    try {
+        const events: Event[] = await sherdog.getEvents()
+        const filteredEvents: Event[] = events.filter((event: Event) => event.name === request.params.name)
+        if(filteredEvents.length !== 1) {
+            response.status(404).json()
+        }
+        const event: Event = filteredEvents[0]
+        response.json(await sherdog.getFights(event))
+    } catch(error) {
+        console.error(error)
+        response.status(500).json()
     }
-    const event: Event = filteredEvents[0]
-    response.json(await sherdog.getFights(event))
 })
 
 router.get('/events', async (request: Request, response: Response<Event[]>) => {
-    response.json(await sherdog.getEvents())
+    try {
+        response.json(await sherdog.getEvents())
+    } catch(error) {
+        console.error(error)
+        response.status(500).json()
+    }
 })
