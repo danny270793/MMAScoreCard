22629c313be1766504ec3c2b4e7c06e249eec8d9
Danny Vaca <danny270793@icloud.com>
2025-03-25 09:10:33 -0500

    add react router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/home';
+import About from './pages/about';
+import NotFound from './pages/404';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 )
