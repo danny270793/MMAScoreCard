229da727608f47a33dbc014208c932e5c7faeaa5
Danny Vaca <danny270793@icloud.com>
2025-03-25 14:16:10 -0500

    improve ui

--- web/src/pages/event.tsx
+++ web/src/pages/event.tsx
@@ -1,16 +1,31 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Event, Fight, Sherdog } from "../services/sherdog";
 import { useEffect, useState } from "react";
+import { Loader } from "../component/loader";
+import { Appbar } from "../component/appbar";
 
 const EventPage = () => {
   const { name } = useParams();
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string|undefined>(undefined)
 
+  const [event, setEvent] = useState<Event|undefined>(undefined)
   const [fights, setFights] = useState<Fight[]>([])
 
   const init = async () => {
-    const event: Event = await Sherdog.getEvent(name!)
-    const fights: Fight[] = await Sherdog.getFights(event)
-    setFights(fights)
+    setLoading(true)
+    try {
+      const event: Event = await Sherdog.getEvent(name!)
+      setEvent(event)
+
+      const fights: Fight[] = await Sherdog.getFights(event)
+      setFights(fights)
+    } catch(error) {
+      console.error(error)
+      setError('Error getting fights')
+    } finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
@@ -18,27 +33,82 @@ const EventPage = () => {
   }, [])
 
   return (
-    <div>
-      <h2>Event {name}</h2>
-            <ul>
-              {fights.map((fight: Fight, index: number) => (
-                <li key={index}>
-                  <div>
+    <>
+      <Appbar title={name!}/>
+      {error && <div className="w3-modal" style={{display: 'block'}}>
+        <div className="w3-modal-content w3-card-4">
+          <div className="w3-container">
+            <span onClick={() => setError(undefined)} className="w3-button w3-display-topright w3-red">
+              &times;
+            </span>
+            <h1>
+              Error
+            </h1>
+            <div>
+              {error}
+            </div>
+            <br/>
+          </div>
+        </div>
+      </div>}
+      <div className="w3-padding">
+        {loading && <Loader/>}
+        {!loading && <>
+          <h5>
+            Event
+          </h5>
+          <div className="w3-card-4 w3-white">
+            <form className="w3-padding">
+              <label>
+                Name
+              </label>
+              <input className="w3-input w3-border" type="text" value={event!.name} disabled/>
+              {event?.figth && <>
+                <label>
+                  Fight
+                </label>
+                <input className="w3-input w3-border" type="text" value={event!.figth!} disabled/>
+              </>}
+              <label>
+                Location
+              </label>
+              <input className="w3-input w3-border" type="text" value={event!.location} disabled/>
+              <label>
+                Date
+              </label>
+              <input className="w3-input w3-border" type="text" value={event!.date.toISOString().split('T')[0]} disabled/>
+            </form>
+          </div>
+
+          <h5>
+            Fights
+          </h5>
+          <ul className="w3-ul w3-card-4 w3-white w3-hoverable">
+            {fights.length === 0 && <li>
+              No fights yet
+            </li>}
+          {fights.map((fight: Fight, index: number) => (
+            <li key={index}>
+              <Link to={`/events/${event!.name}`} style={{textDecoration: 'none'}}>
+                <div>
                   {fight.fighter1.name} vs {fight.fighter2.name}
-                  </div>
-                  <div>
-                  {fight.division}
-                  </div>
-                  <div>
-                  {fight.method} - {fight.referee}
-                  </div>
-                  <div>
+                </div>
+                <div>
+                  {fight.method}
+                </div>
+                <div>
                   Round {fight.round} at {fight.time}
-                  </div>
-                </li>
-              ))}
-            </ul>
-    </div>
+                </div>
+                <div>
+                  {fight.division}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        </>}
+      </div>
+    </>
   );
 };
 
