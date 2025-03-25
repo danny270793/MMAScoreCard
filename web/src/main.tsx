3ea6c695f79c07e2592a91e7f6ea193942ef350a
Danny Vaca <danny270793@icloud.com>
2025-03-25 13:06:47 -0500

    base version

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import NotFound from './pages/404';
-import Events from './pages/events';
-import Event from './pages/event';
+import EventsPage from './pages/events';
+import EventPage from './pages/event';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/events/:id" element={<Event />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/events/:name" element={<EventPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
