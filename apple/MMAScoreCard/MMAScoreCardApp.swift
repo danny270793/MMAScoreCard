865a998bc07a9a727065ba694528bffe2423759d
Danny Vaca <danny270793@icloud.com>
2025-03-27 17:54:10 -0500

    use new component

--- apple/MMAScoreCard/MMAScoreCardApp.swift
+++ apple/MMAScoreCard/MMAScoreCardApp.swift
@@ -10,63 +10,13 @@ import LocalAuthentication
 
 @main
 struct MMAScoreCardApp: App {
-    @State private var error: Error? = nil
     @State var authenticated = false
     @State var authenticating = false
     @State var useFaceId = UserDefaults.standard.bool(forKey: "useFaceId")
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                if useFaceId && !authenticated {
-                    Text("Face ID authentication is required")
-                        .alert(isPresented: .constant(error != nil)) {
-                            Alert(
-                                title: Text("Error"),
-                                message: Text(error!.localizedDescription),
-                                dismissButton: .default(Text("OK")) {
-                                    error = nil
-                                }
-                            )
-                        }
-                        
-                    if authenticating {
-                        ProgressView()
-                    } else {
-                        Button("Authenticate") {
-                            authenticating = true
-                            let context = LAContext()
-                            var error: NSError?
-                            
-                            if !context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-                                self.error = error
-                                authenticating = false
-                                return
-                            }
-                            
-//                            if context.biometryType == .faceID {
-//                                print("Face ID is available.")
-//                            } else if context.biometryType == .touchID {
-//                                print("Touch ID is available.")
-//                            }
-                            
-                            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please authenticate to start ussig the app") { success, error in
-                                DispatchQueue.main.async {
-                                    authenticating = false
-                                    if success {
-                                        self.authenticated = true
-                                    } else {
-                                        self.authenticated = false
-                                        self.error = error
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    EventsList()
-                }
-            }
+            FaceIdUnlock(main: EventsList())
         }
     }
 }
