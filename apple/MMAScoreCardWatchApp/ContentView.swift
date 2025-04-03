2a72e71aadd3a2c8ea189f4f862c3779cc8f137d
Danny Vaca <danny270793@icloud.com>
2025-04-03 17:42:26 -0500

    first screen

--- apple/MMAScoreCardWatchApp/ContentView.swift
+++ apple/MMAScoreCardWatchApp/ContentView.swift
@@ -8,17 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var isFetching: Bool = true
+    @State private var error: Error? = nil
+    @State private var searchText = ""
+    @State var response: SherdogResponse<[Event]>? = nil
+    @State private var filter = FilterOptions.past
+    
+    func onAppear() {
+        Task {
+            await loadEvents(forceRefresh: false)
         }
-        .padding()
+    }
+    
+    func onRefresh() {
+        Task {
+            await loadEvents(forceRefresh: true)
+        }
+    }
+    
+    func loadEvents(forceRefresh: Bool) async {
+        isFetching = true
+        do {
+            response = try await Sheredog.loadEvents(forceRefresh: forceRefresh)
+        } catch {
+            self.error = error
+        }
+        isFetching = false
+    }
+    
+    private var filteredEvents: [Event] {
+        if response == nil {
+            return []
+        }
+        
+        var preFilteredEvents: [Event] = []
+        switch filter {
+        case FilterOptions.all: preFilteredEvents = response!.data
+        case FilterOptions.upcoming: preFilteredEvents = response!.data.filter { event in event.date > Date.now}
+        case FilterOptions.past: preFilteredEvents = response!.data.filter { event in event.date <= Date.now}
+        }
+            
+        if searchText.isEmpty {
+            return preFilteredEvents
+        } else {
+            return preFilteredEvents.filter { event in
+                event.name.lowercased().contains(searchText.lowercased()) ||
+                event.date.ISO8601Format().lowercased().contains(searchText.lowercased()) ||
+                (event.fight ?? "").lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
+    
+    var body: some View {
+        List {
+            ForEach(filteredEvents) { event in
+                VStack {
+                    Text(event.name)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    if event.fight != nil {
+                        Text(event.fight!)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    Text(event.date.ISO8601Format().split(separator: "T")[0])
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            
+            if response?.data != nil || response?.timeCached != nil {
+                Section("Metadata") {
+                    LabeledContent("Cached at", value: response!.cachedAt!.ISO8601Format())
+                    LabeledContent("Time cached", value: response!.timeCached!)
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                NavigationLink(destination: AboutView()) {
+                    Label("About", systemImage: "info")
+                }
+            }
+//            ToolbarItemGroup(placement: .bottomBar) {
+//                Spacer()
+//                Text("\(filteredEvents.count) events")
+//                Spacer()
+//            }
+        }
+        .alert(isPresented: .constant(error != nil)) {
+            Alert(
+                title: Text("Error"),
+                message: Text(error!.localizedDescription),
+                dismissButton: .default(Text("OK")) {
+                    error = nil
+                }
+            )
+        }
+        .overlay {
+            if isFetching {
+                ProgressView()
+            }
+        }
+        .onAppear(perform: onAppear)
+        .refreshable(action: onRefresh)
+        .searchable(text: $searchText)
+        .navigationTitle("Events")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
