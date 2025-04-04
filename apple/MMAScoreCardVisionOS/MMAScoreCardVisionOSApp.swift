db4cadfabdb99701425147f6f761e22c428ecef8
Danny Vaca <danny270793@icloud.com>
2025-04-03 21:22:29 -0500

    vision pro scaffold

---
+++ apple/MMAScoreCardVisionOS/MMAScoreCardVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMAScoreCardVisionOSApp.swift
+//  MMAScoreCardVisionOS
+//
+//  Created by dvaca on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MMAScoreCardVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
