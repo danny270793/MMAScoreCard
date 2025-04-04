7bf6b379352f62e6be648d16e117543e50503c2f
Danny Vaca <danny270793@icloud.com>
2025-04-03 20:22:30 -0500

    apple tv scaffold

---
+++ apple/MMAScoreCardTv/MMAScoreCardTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMAScoreCardTvApp.swift
+//  MMAScoreCardTv
+//
+//  Created by dvaca on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MMAScoreCardTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
