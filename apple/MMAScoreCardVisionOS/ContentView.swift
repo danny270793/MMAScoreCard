e304b4055b40cc3bc65a1d60e1178d012495f716
Danny Vaca <danny270793@icloud.com>
2025-04-03 21:22:29 -0500

    vision pro scaffold

---
+++ apple/MMAScoreCardVisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MMAScoreCardVisionOS
+//
+//  Created by dvaca on 3/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
