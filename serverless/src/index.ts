b878f711a7ae2b34867b52dfe20355265dd3e67e
Danny Vaca <danny270793@icloud.com>
2025-05-31 14:02:22 -0500

    show events

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -2,16 +2,19 @@ import { Cache } from './libraries/cache'
 import Path from 'node:path'
 import { FileCache } from './libraries/cache/file-cache'
 import { Sherdog } from './libraries/sherdog'
+import { Event } from './libraries/sherdog/models/event'
 
 async function main(): Promise<void> {
-    const cachePath: string = Path.join(__dirname, '..', 'assets', 'cache.json')
+    const cachePath: string = Path.join(__dirname, '..', '.cache.json')
     const cache: Cache = new FileCache(cachePath)
 
     const sherdog = new Sherdog()
     sherdog.setCache(cache)
 
-    const events = await sherdog.getEventsFromPage(1)
-    console.log('events')
+    const events: Event[] = await sherdog.getEventsFromPage(1)
+    for (const event of events) {
+        console.log(event)
+    }
 }
 
 main().catch(console.error)
