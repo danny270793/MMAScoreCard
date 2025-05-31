56f17807c24070477195f5655fabe50ababa338d
Danny Vaca <danny270793@icloud.com>
2025-05-31 12:57:11 -0500

    get data

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -1,7 +1,17 @@
 import { Cache } from "./libraries/cache";
 import Path from 'node:path';
 import { FileCache } from "./libraries/cache/file-cache";
+import { Sherdog } from "./libraries/sherdog";
 
-const cachePath: string = Path.join(__dirname, '..', 'assets', 'cache.json');
-const cache: Cache = new FileCache(cachePath);
-console.log('loaded');
+async function main(): Promise<void> {
+    const cachePath: string = Path.join(__dirname, '..', 'assets', 'cache.json');
+    const cache: Cache = new FileCache(cachePath);
+
+    const sherdog = new Sherdog();
+    sherdog.setCache(cache)
+
+    const events = await sherdog.getEventsFromPage(1)
+    console.log('events');
+}
+
+main().catch(console.error)
