56ddb3c4a43742d72dfc8888c3b7c17c37d0c643
Danny Vaca <danny270793@icloud.com>
2025-05-31 12:56:51 -0500

    logger

---
+++ serverless/src/libraries/logger.ts
@@ -0,0 +1,23 @@
+export class Logger {
+    private name: string
+    constructor(name: string) {
+        this.name = name;
+    }
+    write(tag: 'ERROR'|'DEBUG', message: string): void {
+        const output: string = `${new Date()} ${tag} ${this.name} ${message}`
+        if(tag === 'ERROR') {
+            console.error(output);    
+        } else {
+            console.log(output);
+        }
+    }
+    debug(message: string): void {
+        this.write('DEBUG', message)
+    }
+    error(message: string, error?: Error): void {
+        if(error) {
+            console.error(error);
+        }
+        this.write('ERROR', message)
+    }
+}
