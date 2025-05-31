fd7c30094f72725cdc4650a45ff056248ebb9148
Danny Vaca <danny270793@icloud.com>
2025-05-31 12:57:06 -0500

    get data checking on cache

---
+++ serverless/src/libraries/sherdog/index.ts
@@ -0,0 +1,32 @@
+import { Cache } from "../cache"
+import { Logger } from "../logger"
+
+const logger: Logger = new Logger('./libraries/sherdog/index.ts')
+
+export class Sherdog {
+    baseUrl: string = 'https://www.sherdog.com'
+    cache?: Cache = undefined
+
+    setCache(cache?: Cache) {
+        this.cache = cache
+    }
+
+    async getHtml(url: string): Promise<string> {
+        if (this.cache && this.cache.has(url)) {
+            return this.cache.get(url)
+        }
+        logger.debug(`CACHE MISS ${url}`)
+        const html: string = await fetch(url)
+            .then(response => response.text())
+        if(this.cache) {
+            this.cache.set(url, html)
+        }
+        return html
+    }
+
+    async getEventsFromPage(page: number): Promise<string> {
+        const url: string = `${this.baseUrl}/organizations/Ultimate-Fighting-Championship-UFC-2/recent-events/${page}`
+        const html: string = await this.getHtml(url)
+        return html
+    }
+}
