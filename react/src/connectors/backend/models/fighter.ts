121a407733874ec7e4c986076fcd7b48d43b7b92
Danny Vaca <danny270793@icloud.com>
2025-06-03 08:11:26 -0500

    new entity

---
+++ react/src/connectors/backend/models/fighter.ts
@@ -0,0 +1,11 @@
+export interface Fighter {
+    id: string;
+    name: string;
+    nickname?: string
+    cityId: number
+    birthday?: Date,
+    died?: Date
+    height: number
+    weight: number
+    link: string
+}
