d8ae1326db0409aa0d9b068631b8b83aba4d65f0
Danny Vaca <danny270793@icloud.com>
2025-06-02 22:26:39 -0500

    generate id

--- react/src/connector/backend/index.ts
+++ react/src/connector/backend/index.ts
@@ -3,7 +3,7 @@ import type { Event } from "./models/event";
 export class Backend {
     static async getEvents(): Promise<Event[]> {
         return new Promise((resolve) => {
-            setTimeout(() => resolve([{name: 'UFC 300', fight: 'Alex Perira vs Israel Adesanya'}]), 1000);
+            setTimeout(() => resolve([{id: '1', name: 'UFC 300', fight: 'Alex Perira vs Israel Adesanya'}]), 1000);
         });
     }
 }
