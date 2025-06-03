bf8ae1c9ff6b303a2f8c6d62a014c002e146d6b1
Danny Vaca <danny270793@icloud.com>
2025-06-03 08:27:56 -0500

    domain entity

---
+++ react/src/models/fight.ts
@@ -0,0 +1,19 @@
+import type { Category } from "./category"
+import type { Fighter } from "./fighter"
+import type { Referee } from "./referee"
+
+export interface Fight {
+    id: number;
+    position: number
+    category: Category
+    fighterOne: Fighter
+    fighterTwo: Fighter
+    referee: Referee
+    mainEvent: boolean
+    titleFight: boolean
+    type: "pending"|'done'
+    method?: string
+    time?: string
+    round?: number
+    decision?: string
+}
