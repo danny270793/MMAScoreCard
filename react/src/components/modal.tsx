22dcf26400ab7c3f5ff4f6cadbde7825192e51fe
Danny Vaca <danny270793@icloud.com>
2025-06-05 10:54:57 -0500

    allow to define modal type

--- react/src/components/modal.tsx
+++ react/src/components/modal.tsx
@@ -6,22 +6,44 @@ interface ModalProps {
   children?: ReactNode
   title?: string
   onClose?: () => void
+  type?: 'error' | 'info' | 'success'
 }
 
 export const Modal: FC<ModalProps> = (props: ModalProps) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black/70 z-50">
-      <div className="bg-white w-96 shadow-lg rounded-lg">
+      <div
+        className={[
+          'w-96 shadow-lg rounded-lg',
+          props.type === 'error'
+            ? 'bg-red-500'
+            : props.type === 'success'
+              ? 'bg-green-500'
+              : props.type === 'info'
+                ? 'bg-white'
+                : 'bg-white',
+        ].join(' ')}
+      >
         {props.onClose && (
           <div
-            className="float-right px-2 cursor-pointer rounded-tr-lg hover:text-white hover:bg-red-500"
+            className={[
+              'float-right px-2 cursor-pointer rounded-tr-lg hover:bg-red-500',
+              props.type === 'error'
+                ? 'text-white hover:bg-red-800'
+                : 'text-black hover:bg-red-500 hover:text-white',
+            ].join(' ')}
             onClick={props.onClose}
           >
             <FontAwesomeIcon icon={faClose} />
           </div>
         )}
 
-        <div className="p-6">
+        <div
+          className={[
+            'p-6',
+            props.type === 'error' ? 'text-white' : 'text-black',
+          ].join(' ')}
+        >
           {props.title && (
             <div className="text-lg font-semibold mb-4">{props.title}</div>
           )}
