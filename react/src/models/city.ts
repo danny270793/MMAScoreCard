2740e48bad8e9f4ae31ea94fb4a96aeed5d32344
Danny Vaca <danny270793@icloud.com>
2025-06-14 15:21:23 -0500

    add empty model

--- react/src/models/city.ts
+++ react/src/models/city.ts
@@ -1,7 +1,13 @@
-import type { Country } from './country'
+import { EmptyCountry, type Country } from './country'
 
 export interface City {
   id: number
   name: string
   country: Country
+}
+
+export const EmptyCity: City = {
+  id: 0,
+  name: 'city',
+  country: EmptyCountry,
 }
