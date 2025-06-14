339e77587d71c1092787b00da20c08f38d10e1a4
Danny Vaca <danny270793@icloud.com>
2025-06-14 15:23:07 -0500

    get data from supabase

--- react/src/connectors/backend/index.ts
+++ react/src/connectors/backend/index.ts
@@ -1,4 +1,3 @@
-import Events from './assets/events.json'
 import Categories from './assets/categories.json'
 import type { Category } from './models/category'
 import Countries from './assets/countries.json'
@@ -7,15 +6,209 @@ import type { Country } from './models/country'
 import type { City } from './models/city'
 import Fighters from './assets/fighters.json'
 import type { Fighter } from './models/fighter'
-import Fights from './assets/fights.json'
 import type { Fight } from './models/fight'
 import Locations from './assets/locations.json'
 import type { Location } from './models/location'
 import Referees from './assets/referees.json'
 import type { Referee } from './models/referee'
 import type { Event } from './models/event'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { LocalStorageCache } from './cache/local-storage'
+import type { Cache } from './cache'
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
 
 export class Backend {
+  static cache: Cache | undefined = new LocalStorageCache()
+
+  static async getEvents(): Promise<Event[]> {
+    const key: string = 'events'
+    if (Backend.cache && (await Backend.cache.has(key))) {
+      const events: string = await Backend.cache.get(key)
+      return JSON.parse(events)
+    }
+
+    const { data, error } = await supabase
+      .from('events')
+      .select(
+        'id,name,fight,date,link,status,locations(id,name,cities(id,name,countries(id,name)))',
+      )
+      .order('date', { ascending: false })
+    if (error) {
+      throw error
+    }
+    if (Backend.cache) {
+      await Backend.cache.set(key, data)
+    }
+    return data.map((event: unknown) => event as Event)
+  }
+
+  static async getEvent(id: string): Promise<Event> {
+    const key: string = 'event'
+    if (Backend.cache && (await Backend.cache.has(key))) {
+      const event: string = await Backend.cache.get(key)
+      return JSON.parse(event)[0]
+    }
+
+    const { data, error } = await supabase
+      .from('events')
+      .select(
+        'id,name,fight,date,link,status,locations(id,name,cities(id,name,countries(id,name)))',
+      )
+      .eq('id', id)
+    if (error) {
+      throw error
+    }
+    if (Backend.cache) {
+      await Backend.cache.set(key, data)
+    }
+    return data.map((event: unknown) => event as Event)[0]
+  }
+
+  static async getFighter(id: string): Promise<Fighter> {
+    const key: string = `fighter_${id}`
+    if (Backend.cache && (await Backend.cache.has(key))) {
+      const fights: string = await Backend.cache.get(key)
+      return JSON.parse(fights)
+    }
+
+    const { data, error } = await supabase
+      .from('fights')
+      .select(
+        `id,name,nickname,cities(id,name,countries(id,name)),birthday,died,height,weight,link`,
+      )
+      .eq('id', id)
+    if (error) {
+      throw error
+    }
+    if (Backend.cache) {
+      await Backend.cache.set(key, data)
+    }
+    return data.map((fighter: unknown) => fighter as Fighter)[0]
+  }
+
+  static async getFights(eventId: string): Promise<Fight[]> {
+    const key: string = `fights_${eventId}`
+    if (Backend.cache && (await Backend.cache.has(key))) {
+      const fights: string = await Backend.cache.get(key)
+      return JSON.parse(fights)
+    }
+
+    const { data, error } = await supabase
+      .from('fights_with_figthers')
+      .select(
+        `id,position,
+        categories(id,name,weight),
+        referees(id,name),
+        fightertwoid,fightertwoname,fightertwonickname,fightertwobirthday,fightertwodied,fightertwoheight,fightertwoweight,fightertwolink,
+        fighteroneid,fighteronename,fighteronenickname,fighteronebirthday,fighteronedied,fighteroneheight,fighteroneweight,fighteronelink,
+        mainEvent,titleFight,type,method,time,round,decision,
+        events(id,name,fight,date,link,status,locations(id,name,cities(id,name,countries(id,name))))`,
+      )
+      .eq('eventId', eventId)
+      .order('position', { ascending: false })
+    if (error) {
+      throw error
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const parsedData: Fight[] = data.map<Fight>((fight: any) => {
+      return {
+        id: fight.id,
+        position: fight.position,
+        categories: {
+          id: fight.categories.id,
+          name: fight.categories.name,
+          weight: fight.categories.weight,
+        } as Category,
+        fighterOne: {
+          id: fight.fighteroneid,
+          name: fight.fighteronename,
+          nickname: fight.fighteronenickname,
+          cities: {
+            id: fight.fighteronecityid,
+            name: fight.fighteronecityname,
+            countries: {
+              id: fight.fighteronecountryid,
+              name: fight.fighteronecountryname,
+            } as Country,
+          } as City,
+          birthday: fight.fighteronebirthday
+            ? new Date(fight.fighteronebirthday)
+            : undefined,
+          died: fight.fighteronedied
+            ? new Date(fight.fighteronedied)
+            : undefined,
+          height: fight.fighteroneheight,
+          weight: fight.fighteroneweight,
+          link: fight.fighteronelink,
+        } as Fighter,
+        fighterTwo: {
+          id: fight.fightertwoid,
+          name: fight.fightertwoname,
+          nickname: fight.fightertwonickname,
+          cities: {
+            id: fight.fightertwocityid,
+            name: fight.fightertwocityname,
+            countries: {
+              id: fight.fightertwocountryid,
+              name: fight.fightertwocountryname,
+            } as Country,
+          } as City,
+          birthday: fight.fightertwobirthday
+            ? new Date(fight.fightertwobirthday)
+            : undefined,
+          died: fight.fightertwodied
+            ? new Date(fight.fightertwodied)
+            : undefined,
+          height: fight.fightertwoheight,
+          weight: fight.fightertwoweight,
+          link: fight.fightertwolink,
+        } as Fighter,
+        referees: fight.referees
+          ? ({
+              id: fight.referees.id,
+              name: fight.referees.name,
+            } as Referee)
+          : undefined,
+        mainEvent: fight.mainEvent,
+        titleFight: fight.titleFight,
+        type: fight.type,
+        method: fight.method,
+        time: fight.time,
+        round: fight.round,
+        decision: fight.decision,
+        events: {
+          id: fight.events.id,
+          name: fight.events.name,
+          fight: fight.events.fight,
+          date: fight.events.date,
+          link: fight.events.link,
+          locations: {
+            id: fight.events.locations.id,
+            name: fight.events.locations.name,
+            cities: {
+              id: fight.events.locations.cities.id,
+              name: fight.events.locations.cities.name,
+              countries: {
+                id: fight.events.locations.cities.countries.id,
+                name: fight.events.locations.cities.countries.name,
+              } as Country,
+            } as City,
+          } as Location,
+          status: fight.events.status,
+        } as Event,
+      }
+    })
+
+    if (Backend.cache) {
+      await Backend.cache.set(key, parsedData)
+    }
+    return parsedData
+  }
+
   static async getCategories(): Promise<Category[]> {
     return Categories.map<Category>((category: unknown) => category as Category)
   }
@@ -25,16 +218,8 @@ export class Backend {
   static async getCountries(): Promise<Country[]> {
     return Countries.map<Country>((country: unknown) => country as Country)
   }
-  static async getEvents(): Promise<Event[]> {
-    return Events.map<Event>((event: unknown) => event as Event)
-  }
   static async getFighters(): Promise<Fighter[]> {
     return Fighters.map<Fighter>((fighter: unknown) => fighter as Fighter)
-  }
-  static async getFights(id: string): Promise<Fight[]> {
-    return Fights.map<Fight>((fight: unknown) => fight as Fight).filter(
-      (fight: Fight) => `${fight.eventId}` === id,
-    )
   }
   static async getLocations(): Promise<Location[]> {
     return Locations.map<Location>((location: unknown) => location as Location)
