a93ebf74031cd431a371cfde06ed9024fcb17f99
Danny Vaca <danny270793@icloud.com>
2025-06-14 15:55:11 -0500

    revemo no needed

--- react/src/connectors/backend/index.ts
+++ react/src/connectors/backend/index.ts
@@ -1,15 +1,9 @@
-import Categories from './assets/categories.json'
 import type { Category } from './models/category'
-import Countries from './assets/countries.json'
-import Cities from './assets/cities.json'
 import type { Country } from './models/country'
 import type { City } from './models/city'
-import Fighters from './assets/fighters.json'
 import type { Fighter } from './models/fighter'
 import type { Fight } from './models/fight'
-import Locations from './assets/locations.json'
 import type { Location } from './models/location'
-import Referees from './assets/referees.json'
 import type { Referee } from './models/referee'
 import type { Event } from './models/event'
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
@@ -326,24 +320,5 @@ export class Backend {
       await Backend.cache.set(key, parsedData)
     }
     return parsedData
-  }
-
-  static async getCategories(): Promise<Category[]> {
-    return Categories.map<Category>((category: unknown) => category as Category)
-  }
-  static async getCities(): Promise<City[]> {
-    return Cities.map<City>((city: unknown) => city as City)
-  }
-  static async getCountries(): Promise<Country[]> {
-    return Countries.map<Country>((country: unknown) => country as Country)
-  }
-  static async getFighters(): Promise<Fighter[]> {
-    return Fighters.map<Fighter>((fighter: unknown) => fighter as Fighter)
-  }
-  static async getLocations(): Promise<Location[]> {
-    return Locations.map<Location>((location: unknown) => location as Location)
-  }
-  static async getReferees(): Promise<Referee[]> {
-    return Referees.map<Referee>((referee: unknown) => referee as Referee)
   }
 }
