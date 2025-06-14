9d45c67bc93a9881b9b5d12787ec9e7e43594f6c
Danny Vaca <danny270793@icloud.com>
2025-06-14 15:22:11 -0500

    improve skeleton

--- react/src/pages/events.tsx
+++ react/src/pages/events.tsx
@@ -119,58 +119,67 @@ export const EventsPage: FC = () => {
               </div>
             </ListItem>
           ))}
-        {events.length === 0 && (
+        {state !== 'getting_events' && events.length === 0 && (
           <ListItem
             subtitle={t('eventsNotFound', { postProcess: 'capitalize' })}
           />
         )}
-        {events.map((event: Event) => (
-          <ListItem
-            title={event.name}
-            subtitle={event.fight}
-            key={event.id}
-            link={`/events/${event.id}`}
-            chevronCenter
-            after={
-              event.status === 'uppcoming'
-                ? t('upcoming', { postProcess: 'capitalize' })
-                : ''
-            }
-          >
-            <br />
-            <div>
-              <FontAwesomeIcon className="w-4" icon={faCalendar} />{' '}
-              <span className="opacity-60">
-                {event.date.toISOString().split('T')[0]}
-              </span>{' '}
-              {event.status === 'uppcoming' && (
+        {state !== 'getting_events' &&
+          events.map((event: Event) => (
+            <ListItem
+              title={event.name}
+              subtitle={event.fight}
+              key={event.id}
+              link={`/events/${event.id}`}
+              chevronCenter
+              after={
+                event.status === 'uppcoming'
+                  ? t('upcoming', { postProcess: 'capitalize' })
+                  : ''
+              }
+            >
+              <br />
+              <div>
+                <FontAwesomeIcon className="w-4" icon={faCalendar} />{' '}
                 <span className="opacity-60">
-                  {t('inXXDays', {
-                    days: DateUtils.daysBetween(event.date, new Date()),
-                  })}
+                  {event.date.toISOString().split('T')[0]}
+                </span>{' '}
+                {event.status === 'uppcoming' && (
+                  <span className="opacity-60">
+                    {t('inXXDays', {
+                      days: DateUtils.daysBetween(event.date, new Date()),
+                    })}
+                  </span>
+                )}
+              </div>
+              <div>
+                <FontAwesomeIcon className="w-4" icon={faMap} />{' '}
+                <span className="opacity-60">
+                  {event.location.city.country.name} -{' '}
+                  {event.location.city.name}
                 </span>
-              )}
-            </div>
-            <div>
-              <FontAwesomeIcon className="w-4" icon={faMap} />{' '}
-              <span className="opacity-60">
-                {event.location.city.country.name} - {event.location.city.name}
-              </span>
-            </div>
-            <div>
-              {event.location.name !== '' && (
-                <>
-                  <FontAwesomeIcon className="w-4" icon={faLocation} />{' '}
-                  <span className="opacity-60">{event.location.name}</span>
-                </>
-              )}
-            </div>
-          </ListItem>
-        ))}
+              </div>
+              <div>
+                {event.location.name !== '' && (
+                  <>
+                    <FontAwesomeIcon className="w-4" icon={faLocation} />{' '}
+                    <span className="opacity-60">{event.location.name}</span>
+                  </>
+                )}
+              </div>
+            </ListItem>
+          ))}
       </List>
       <Toolbar bottom>
         <div />
-        <div>{t('eventsCounter', { events: events.length })}</div>
+        {state === 'getting_events' && (
+          <div className="skeleton-text skeleton-effect-wave">
+            {t('eventsCounter', { events: 0 })}
+          </div>
+        )}
+        {state !== 'getting_events' && (
+          <div>{t('eventsCounter', { events: events.length })}</div>
+        )}
         <div />
       </Toolbar>
     </Page>
