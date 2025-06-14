7b5104dedd945deaa81d89a11bba193a2a0ef274
Danny Vaca <danny270793@icloud.com>
2025-06-14 09:40:31 -0500

    remove old

--- backend/src/libraries/cache/index.ts
+++
@@ -1,5 +0,0 @@
-export interface Cache {
-    has(key: string): Promise<boolean>
-    get(key: string): Promise<string|null>
-    set(key: string, value: string): Promise<void>
-}
--- web/src/component/loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-export const Loader = () => {
-    return (
-        <div className="spinner-container">
-            <div className="spinner"/>
-        </div>
-    )
-}
