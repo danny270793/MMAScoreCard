f9b620cadd7835ad9214c9ceb6407dcc10e76605
Danny Vaca <danny270793@icloud.com>
2025-06-16 11:18:19 -0500

    cache data

---
+++ crawler/src/libraries/cache/memory-cache.ts
@@ -0,0 +1,18 @@
+import { Cache } from '.'
+
+export class MemoryCache implements Cache {
+    private data: any = {}
+
+    keysCount(): number {
+        return Object.keys(this.data).length
+    }
+    has(key: string): boolean {
+        return this.data.hasOwnProperty(key)
+    }
+    get(key: string): any {
+        return this.data[key]
+    }
+    set(key: string, value: any): void {
+        this.data[key] = value
+    }
+}
