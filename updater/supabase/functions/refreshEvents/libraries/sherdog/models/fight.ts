f8959eb03001fb0af8a07c5102fe8db63f3532d7
Danny Vaca <danny270793@icloud.com>
2025-06-16 14:46:03 -0500

    format code

--- updater/supabase/functions/refreshEvents/libraries/sherdog/models/fight.ts
+++ updater/supabase/functions/refreshEvents/libraries/sherdog/models/fight.ts
@@ -1,44 +1,44 @@
 export interface Fighter {
-    name: string
-    link: string
-    result?: string
+  name: string;
+  link: string;
+  result?: string;
 }
 
 export interface Category {
-    name: string
-    weight?: number
+  name: string;
+  weight?: number;
 }
 
 export interface BaseFight {
-    position: number
-    fighterOne: Fighter
-    category?: Category
-    fighterTwo: Fighter
-    mainEvent: boolean
-    titleFight: boolean
+  position: number;
+  fighterOne: Fighter;
+  category?: Category;
+  fighterTwo: Fighter;
+  mainEvent: boolean;
+  titleFight: boolean;
 }
 
 export interface PendingFight extends BaseFight {
-    type: 'pending'
+  type: "pending";
 }
 
 export interface NoEventFight {
-    fighter: Fighter
-    decision?: string
-    referee?: string
-    method?: string
-    time: number
-    round: number
-    proFight: boolean
+  fighter: Fighter;
+  decision?: string;
+  referee?: string;
+  method?: string;
+  time: number;
+  round: number;
+  proFight: boolean;
 }
 
 export interface DoneFight extends BaseFight {
-    decision?: string
-    referee: string
-    method?: string
-    time: number
-    round: number
-    type: 'done'
+  decision?: string;
+  referee: string;
+  method?: string;
+  time: number;
+  round: number;
+  type: "done";
 }
 
-export type Fight = PendingFight | DoneFight
+export type Fight = PendingFight | DoneFight;
