ce811702d0a4845439f85aa7e8c0416f67588cf0
Danny Vaca <danny270793@icloud.com>
2025-06-16 14:46:07 -0500

    format code

--- updater/supabase/functions/refreshEvents/libraries/sherdog/models/stats.ts
+++ updater/supabase/functions/refreshEvents/libraries/sherdog/models/stats.ts
@@ -1,9 +1,9 @@
 export interface Stats {
-    nickname?: string
-    country: string
-    city: string
-    birthday?: Date
-    died?: Date
-    height: number
-    weight: number
+  nickname?: string;
+  country: string;
+  city: string;
+  birthday?: Date;
+  died?: Date;
+  height: number;
+  weight: number;
 }
