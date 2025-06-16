a3387f74f5447e25a09bb0f01a07384c5a20ea06
Danny Vaca <danny270793@icloud.com>
2025-06-16 14:46:12 -0500

    format code

--- updater/supabase/functions/refreshEvents/libraries/logger.ts
+++ updater/supabase/functions/refreshEvents/libraries/logger.ts
@@ -1,33 +1,35 @@
-import { Utils } from './utils.ts'
+import { Utils } from "./utils.ts";
 
 export class Logger {
-    public static enabled: boolean = true
+  public static enabled: boolean = true;
 
-    private name: string
-    private enable: boolean
-    constructor(name: string, enable: boolean = true) {
-        this.name = name
-        this.enable = enable
+  private name: string;
+  private enable: boolean;
+  constructor(name: string, enable: boolean = true) {
+    this.name = name;
+    this.enable = enable;
+  }
+  write(tag: "ERROR" | "DEBUG", message: string): void {
+    if (!this.enable || !Logger.enabled) {
+      return;
     }
-    write(tag: 'ERROR' | 'DEBUG', message: string): void {
-        if (!this.enable || !Logger.enabled) {
-            return
-        }
 
-        const output: string = `${Utils.toYYYYMMDDHHMMSSUUUU(new Date())} ${tag} ${this.name} ${message}`
-        if (tag === 'ERROR') {
-            console.error(output)
-        } else {
-            console.log(output)
-        }
+    const output: string = `${Utils.toYYYYMMDDHHMMSSUUUU(new Date())} ${tag} ${
+      this.name
+    } ${message}`;
+    if (tag === "ERROR") {
+      console.error(output);
+    } else {
+      console.log(output);
     }
-    debug(message: string): void {
-        this.write('DEBUG', message)
+  }
+  debug(message: string): void {
+    this.write("DEBUG", message);
+  }
+  error(message: string, error?: Error): void {
+    if (error) {
+      console.error(error);
     }
-    error(message: string, error?: Error): void {
-        if (error) {
-            console.error(error)
-        }
-        this.write('ERROR', message)
-    }
+    this.write("ERROR", message);
+  }
 }
