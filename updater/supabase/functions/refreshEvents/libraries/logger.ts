e7e121e8785e5fd7630165487d2fa23708ae48d3
Danny Vaca <danny270793@icloud.com>
2025-06-15 22:01:47 -0500

    logger

---
+++ updater/supabase/functions/refreshEvents/libraries/logger.ts
@@ -0,0 +1,33 @@
+import { Utils } from './utils.ts'
+
+export class Logger {
+    public static enabled: boolean = true
+
+    private name: string
+    private enable: boolean
+    constructor(name: string, enable: boolean = true) {
+        this.name = name
+        this.enable = enable
+    }
+    write(tag: 'ERROR' | 'DEBUG', message: string): void {
+        if (!this.enable || !Logger.enabled) {
+            return
+        }
+
+        const output: string = `${Utils.toYYYYMMDDHHMMSSUUUU(new Date())} ${tag} ${this.name} ${message}`
+        if (tag === 'ERROR') {
+            console.error(output)
+        } else {
+            console.log(output)
+        }
+    }
+    debug(message: string): void {
+        this.write('DEBUG', message)
+    }
+    error(message: string, error?: Error): void {
+        if (error) {
+            console.error(error)
+        }
+        this.write('ERROR', message)
+    }
+}
