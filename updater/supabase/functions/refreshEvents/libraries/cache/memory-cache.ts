a2ea484294bd9598de5dfe9723a9b730653f3f33
Danny Vaca <danny270793@icloud.com>
2025-06-16 14:45:59 -0500

    format code

--- updater/supabase/functions/refreshEvents/libraries/cache/memory-cache.ts
+++ updater/supabase/functions/refreshEvents/libraries/cache/memory-cache.ts
@@ -1,18 +1,18 @@
-import { Cache } from './index.ts'
+import { Cache } from "./index.ts";
 
 export class MemoryCache implements Cache {
-    private data: any = {}
+  private data: any = {};
 
-    keysCount(): number {
-        return Object.keys(this.data).length
-    }
-    has(key: string): boolean {
-        return this.data.hasOwnProperty(key)
-    }
-    get(key: string): any {
-        return this.data[key]
-    }
-    set(key: string, value: any): void {
-        this.data[key] = value
-    }
+  keysCount(): number {
+    return Object.keys(this.data).length;
+  }
+  has(key: string): boolean {
+    return this.data.hasOwnProperty(key);
+  }
+  get(key: string): any {
+    return this.data[key];
+  }
+  set(key: string, value: any): void {
+    this.data[key] = value;
+  }
 }
