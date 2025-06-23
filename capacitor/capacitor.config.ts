ac291dbd577960887685ef808238d2647b67c040
Danny Vaca <danny270793@icloud.com>
2025-06-23 11:51:24 -0500

    format correctly

--- capacitor/capacitor.config.ts
+++ capacitor/capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  "appId": "io.github.danny270793.mmascorecard",
-  "appName": "MMA ScoreCard",
-  "webDir": "www",
+  appId: "io.github.danny270793.mmascorecard",
+  appName: "MMA ScoreCard",
+  webDir: "www",
   plugins: {
     SplashScreen: {
       backgroundColor: "#ff0166da",
