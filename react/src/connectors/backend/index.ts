5c48b03e0992473c7c201989fb4c07364b2845fc
Danny Vaca <danny270793@icloud.com>
2025-06-22 20:55:22 -0500

    make it optional

--- react/src/connectors/backend/index.ts
+++ react/src/connectors/backend/index.ts
@@ -232,11 +232,13 @@ export class Backend {
         id: fight.id,
         winner: fight.winner,
         position: fight.position,
-        categories: {
-          id: fight.categories.id,
-          name: fight.categories.name,
-          weight: fight.categories.weight,
-        } as Category,
+        categories:
+          fight.categories &&
+          ({
+            id: fight.categories.id,
+            name: fight.categories.name,
+            weight: fight.categories.weight,
+          } as Category),
         fighterOne: {
           id: fight.fighteroneid,
           name: fight.fighteronename,
