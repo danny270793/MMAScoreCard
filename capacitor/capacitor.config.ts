e865ac00f63e13244484291fcd82ab4e762c57d9
Danny Vaca <danny270793@icloud.com>
2025-06-23 11:51:00 -0500

    add plugin confioguration

--- capacitor/capacitor.config.ts
+++ capacitor/capacitor.config.ts
@@ -3,7 +3,12 @@ import { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   "appId": "io.github.danny270793.mmascorecard",
   "appName": "MMA ScoreCard",
-  "webDir": "www"
+  "webDir": "www",
+  plugins: {
+    SplashScreen: {
+      backgroundColor: "#ff0166da",
+    }
+  }
 }
 
 export default config;
