e75d1a7be555cde6bd1ab873b4123a87e5d69fc2
Danny Vaca <danny270793@icloud.com>
2025-06-23 08:19:17 -0500

    show fights

--- react/src/pages/fighter.tsx
+++ react/src/pages/fighter.tsx
@@ -9,6 +9,17 @@ import { useTranslation } from 'react-i18next'
 import type { Dispatch } from '@reduxjs/toolkit'
 import { Logger } from '../utils/logger'
 import { EmptyFighter, type Fighter } from '../models/fighter'
+import type { Fight } from '../models/fight'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { TranslationUtils } from '../utils/translations'
+import {
+  faClock,
+  faPersonChalkboard,
+  faThumbsDown,
+  faThumbsUp,
+  faWeight,
+} from '@fortawesome/free-solid-svg-icons'
+import { DateUtils } from '../utils/date-utils'
 
 const logger: Logger = new Logger('/src/pages/fighter.tsx')
 
@@ -21,6 +32,7 @@ export const FighterPage: FC<FighterPageProps> = (props: FighterPageProps) => {
   const dispatch: Dispatch = useDispatch()
 
   const fighter: Fighter | undefined = useSelector(backendSelectors.getFighter)
+  const fights: Fight[] = useSelector(backendSelectors.getFights)
   const error: Error | undefined = useSelector(backendSelectors.getError)
   const state: string = useSelector(backendSelectors.getState)
 
@@ -55,6 +67,29 @@ export const FighterPage: FC<FighterPageProps> = (props: FighterPageProps) => {
     const feet: number = Math.floor(totalInches / 12)
     const inches: number = Math.round(totalInches % 12)
     return `${feet}'${inches}''`
+  }
+
+  const getWins = (fighter: Fighter, fights: Fight[]): number => {
+    return fights.filter(
+      (fight) =>
+        (fight.winner === 1 && fight.fighterOne.id === fighter.id) ||
+        (fight.winner === 2 && fight.fighterTwo.id === fighter.id),
+    ).length
+  }
+  const getLosses = (fighter: Fighter, fights: Fight[]): number => {
+    return fights.filter(
+      (fight) =>
+        (fight.winner === 2 && fight.fighterOne.id === fighter.id) ||
+        (fight.winner === 1 && fight.fighterTwo.id === fighter.id),
+    ).length
+  }
+
+  const getDraws = (fights: Fight[]): number => {
+    return fights.filter((fight) => fight.decision === 'Draw').length
+  }
+
+  const getNoContest = (fights: Fight[]): number => {
+    return fights.filter((fight) => fight.decision === 'No Contest').length
   }
 
   return (
@@ -142,6 +177,134 @@ export const FighterPage: FC<FighterPageProps> = (props: FighterPageProps) => {
           </ListItem>
         </List>
       )}
+
+      <BlockTitle>{t('record', { postProcess: 'capitalize' })}</BlockTitle>
+      {state !== 'getting_fighter' && fighter && (
+        <List dividersIos mediaList strongIos inset>
+          <ListItem
+            after={`${getWins(fighter, fights)}`}
+            title={t('wins', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={`${getLosses(fighter, fights)}`}
+            title={t('losses', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={`${getDraws(fights)}`}
+            title={t('draws', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={`${getNoContest(fights)}`}
+            title={t('noContest', { postProcess: 'capitalize' })}
+          />
+        </List>
+      )}
+      {state === 'getting_fighter' && (
+        <List
+          className="skeleton-text skeleton-effect-wave"
+          dividersIos
+          mediaList
+          strongIos
+          inset
+        >
+          <ListItem
+            after={1}
+            title={t('wins', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={1}
+            title={t('losses', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={1}
+            title={t('draws', { postProcess: 'capitalize' })}
+          />
+          <ListItem
+            after={1}
+            title={t('noContest', { postProcess: 'capitalize' })}
+          />
+        </List>
+      )}
+      <BlockTitle>{t('fights', { postProcess: 'capitalize' })}</BlockTitle>
+      <div className="timeline">
+        {fighter &&
+          fights.map((fight: Fight) => (
+            <div key={fight.id} className="timeline-item">
+              <div className="timeline-item-date">
+                {fight.event.date.toISOString().split('T')[0]}
+              </div>
+              <div className="timeline-item-divider" />
+              <div className="timeline-item-content">
+                {/* <div className="timeline-item-inner">Some text goes here</div> */}
+                <List dividersIos mediaList strongIos inset>
+                  <ListItem
+                    key={fight.id}
+                    title={
+                      fight.fighterTwo.id === fighter.id
+                        ? fight.fighterOne.name
+                        : fight.fighterTwo.name
+                    }
+                    subtitle={
+                      fight.titleFight
+                        ? t('titleFight', { postProcess: 'capitalize' })
+                        : ''
+                    }
+                  >
+                    <span slot="after">
+                      {fight.fighterOne.id === fighter.id && (
+                        <FontAwesomeIcon className="w-4" icon={faThumbsUp} />
+                      )}
+                      {fight.fighterTwo.id === fighter.id && (
+                        <FontAwesomeIcon className="w-4" icon={faThumbsDown} />
+                      )}
+                    </span>
+                    <br />
+                    {fight.category && (
+                      <div>
+                        <FontAwesomeIcon className="w-4" icon={faWeight} />{' '}
+                        {TranslationUtils.getCategoryName(
+                          t,
+                          fight.category.name,
+                        )}{' '}
+                        ({fight.category.weight} lbs)
+                      </div>
+                    )}
+                    {fight.type === 'done' && (
+                      <>
+                        <div>
+                          <FontAwesomeIcon
+                            className="w-4"
+                            icon={faPersonChalkboard}
+                          />{' '}
+                          {fight.decision &&
+                            TranslationUtils.getDecisionName(
+                              t,
+                              fight.decision,
+                            )}{' '}
+                          (
+                          {fight.method &&
+                            TranslationUtils.getDecisionMethodName(
+                              t,
+                              fight.method,
+                            )}
+                          )
+                        </div>
+                        {fight.decision !== 'Decision' && (
+                          <div>
+                            <FontAwesomeIcon className="w-4" icon={faClock} />{' '}
+                            {t('round', { postProcess: 'capitalize' })}{' '}
+                            {fight.round} {t('at')}{' '}
+                            {DateUtils.secondsToMMSS(fight.time!)}
+                          </div>
+                        )}
+                      </>
+                    )}
+                  </ListItem>
+                </List>
+              </div>
+            </div>
+          ))}
+      </div>
     </Page>
   )
 }
