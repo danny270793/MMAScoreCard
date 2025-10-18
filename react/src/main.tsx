51312dbce1a2bafa242d2e32e7d030e16e218def
Danny Vaca <danny270793@icloud.com>
2025-10-18 14:33:36 -0500

    Remove Framework7 imports and initialization from main.tsx
    - Remove Framework7 and Framework7React imports that are no longer needed
    - Remove Framework7.use(Framework7React) initialization
    - Clean up main.tsx after migration to React Router and Tailwind CSS
    - Maintain all other functionality including Redux store, styles, and i18n

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -4,11 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './reducers/index.ts'
 import './styles/index.css'
 import './i18n'
-import Framework7 from 'framework7/lite-bundle'
-import Framework7React from 'framework7-react'
 import { DarkMode } from './components/dark-mode.tsx'
-
-Framework7.use(Framework7React)
 
 if (window.location.href.startsWith('capacitor://')) {
   if (!window.location.href.startsWith('capacitor://localhost/')) {
